8e777cd26ae80e7331402254bb6bdcc468527898
Alexander Vorontsov <sas@localhost.localdomain>
2015-10-11 09:12:11 +0500

    add history module and new program

---
+++ src/history.f90
@@ -0,0 +1,242 @@
+module history
+
+   implicit none
+
+   integer, private :: max_events = 0, n_events = 0
+
+   type event 
+      integer, dimension(:), allocatable :: part1,  part2   ! части кластера
+      integer :: n1, n2		!размеры частей n1 >= n2
+      integer :: time		!время столкновения или время разваливания
+      integer :: fusion		!if grow == 1 - fusion, if grow == -1 - dissociation
+   end type event
+
+   type(event), dimension(:), pointer :: all_events
+   
+   private :: grow_all_events
+
+contains
+!--------------------------------------------
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+subroutine add_fusion(new_p,new_cl,old_cl,time)
+
+   implicit none
+   integer :: new_cl(*),old_cl(*), time
+   integer :: old1_cl(1000), i,j,k
+   type(event), pointer :: new_p
+    
+!   write(*,*) 'hist: add fusion procedure', time, new_cl(1:5),old_cl(1:5)
+   i = 1
+   j = 1
+   k = 1
+   do while (new_cl(i) > 0)
+     if (new_cl(i) == old_cl(j)) then 
+       j = j + 1
+     else
+       old1_cl(k) = new_cl(i)
+       k = k + 1
+     endif
+     i = i + 1
+   enddo
+    write(*,*) 'hist: fusion: size1 = ', j-1, old_cl(1:j-1),' size2 = ', k-1, old1_cl(1:k-1)
+
+   if (n_events+1 .ge. max_events) then 
+      call grow_all_events(100)
+   endif
+   n_events = n_events + 1
+   new_p => all_events(n_events)
+   
+   if (j .gt. k) then 
+      allocate(new_p%part1(j-1))
+      allocate(new_p%part2(k-1))
+      new_p%part1(:) = old_cl(1:j-1)
+      new_p%part2(:) = old1_cl(1:k-1)
+      new_p%n1 = j-1
+      new_p%n2 = k-1
+   else
+      allocate(new_p%part1(k-1))
+      allocate(new_p%part2(j-1))
+      new_p%part2(:) = old_cl(1:j-1)
+      new_p%part1(:) = old1_cl(1:k-1)
+      new_p%n2 = j-1
+      new_p%n1 = k-1
+   endif
+   new_p%time = time
+   new_p%fusion = 1
+
+end subroutine add_fusion
+!----------------------------------------------
+subroutine add_diss(new_p,new_cl,old_cl,time)
+
+   implicit none
+   integer :: new_cl(*),old_cl(*), time
+   integer :: new1_cl(1000), i,j,k
+   type(event), pointer :: new_p
+!   write(*,*) 'hist: add dissociation procedure', time, new_cl(1:5),old_cl(1:5)
+
+   i = 1
+   j = 1
+   k = 1
+   do while (old_cl(i) > 0)
+     if (old_cl(i) == new_cl(j)) then 
+       j = j + 1
+     else
+       new1_cl(k) = old_cl(i)
+       k = k + 1
+     endif
+     i = i + 1
+   enddo
+   write(*,*) 'hist: dissociation:  size1 = ', j-1, new_cl(1:j-1),' size2 = ', k-1, new1_cl(1:k-1)
+   
+   if (n_events+1 .ge. max_events) then 
+      call grow_all_events(100)
+   endif
+   n_events = n_events + 1
+   new_p => all_events(n_events)
+
+   if (j .gt. k) then 
+      allocate(new_p%part1(j-1))
+      allocate(new_p%part2(k-1))
+      new_p%part1(:) = new_cl(1:j-1)
+      new_p%part2(:) = new1_cl(1:k-1)
+      new_p%n1 = j-1
+      new_p%n2 = k-1
+   else
+      allocate(new_p%part1(k-1))
+      allocate(new_p%part2(j-1))
+      new_p%part2(:) = new_cl(1:j-1)
+      new_p%part1(:) = new1_cl(1:k-1)
+      new_p%n2 = j-1
+      new_p%n1 = k-1
+   endif
+   new_p%time = time
+   new_p%fusion = -1 
+   
+end subroutine add_diss
+!----------------------------------------------
+subroutine rm_event(ev)
+
+   implicit none
+   type(event), pointer :: ev
+   integer :: i
+   
+   if (.not. associated(ev)) then
+     write(*,*) 'hist: warning try delete bad event' 
+     return
+   endif
+   write(*,*) 'hist: remove event',n_events
+   
+   i = 1
+   do while (i .le. n_events)
+      if ( associated(ev, all_events(i)) ) then
+         
+!         write(*,*) 'hist: ', ev%part1,ev%part2
+      
+         write(*,*) 'hist: del event nimber', i
+         deallocate(ev%part1)
+         deallocate(ev%part2)
+
+         n_events = n_events -1
+         do while (i .le. n_events)
+            all_events(i) = all_events(i+1)
+            i = i+1
+         enddo
+         deallocate(all_events(n_events+1)%part1)
+         deallocate(all_events(n_events+1)%part2)
+!         deallocate(ev)
+         nullify(ev)
+         return
+      endif 
+      i = i+1
+   enddo
+   write(*,*) 'hist: bad pointer. No event'
+
+end subroutine rm_event
+!----------------------------------------------
+
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+integer function loop0(ev1,ev2)
+
+   implicit none
+   type(event), pointer :: ev1,ev2
+   integer :: i
+!   write(*,*) 'hist: Check for zero loop'
+
+   loop0 = 0
+   if (.not. associated(ev1)) return
+   if (.not. associated(ev2)) return
+   if (ev1%part1(1) .eq. ev2%part1(1)) then
+        if (size(ev1%part1(:)) .ne. size(ev2%part1(:))) return
+        do i=1, size(ev1%part1(:))
+          if (ev1%part1(i) .ne. ev2%part1(i)) return
+        enddo 
+        
+        if (size(ev1%part2(:)) .ne. size(ev2%part2(:))) return
+        do i=1, size(ev1%part2(:))
+          if (ev1%part2(i) .ne. ev2%part2(i)) return
+        enddo 
+    else if (ev1%part1(1) .eq. ev2%part2(1)) then
+        if (size(ev1%part1(:)) .ne. size(ev2%part2(:))) return
+        do i=1, size(ev1%part1(:))
+          if (ev1%part1(i) .ne. ev2%part2(i)) return
+        enddo 
+        
+        if (size(ev1%part2(:)) .ne. size(ev2%part1(:))) return
+        do i=1, size(ev1%part2(:))
+          if (ev1%part2(i) .ne. ev2%part1(i)) return
+        enddo 
+    
+    else 
+        return
+    endif
+    loop0 = 1
+end function loop0
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+integer function loop1(ev1,ev2,ev3)
+
+   implicit none
+   type(event), pointer :: ev1,ev2,ev3
+   integer :: i
+
+   loop1 = 0
+   if (.not. associated(ev1)) return
+   if (.not. associated(ev2)) return
+   if (.not. associated(ev3)) return
+
+
+end function loop1
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+subroutine grow_all_events(n)
+
+   implicit none
+   integer n
+   type(event),  dimension(:), allocatable :: buf
+  
+   write(*,*) 'hist: start grow all_events array', max_events, n_events
+   if (associated(all_events)) then 
+      allocate(buf(size(all_events(:))))
+      buf(:) = all_events(:)
+      deallocate(all_events)
+   endif
+   
+   max_events = max_events + n
+   allocate(all_events(max_events))
+     
+   if (allocated(buf)) then 
+      all_events(:) =  buf(:)
+      deallocate(buf)
+   endif
+   write(*,*) 'hist: finalize grow all_events array', max_events, n_events
+
+end subroutine grow_all_events
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11111
+
+
+
+
+
+end module history
---
+++ src/prog_test.f90
@@ -0,0 +1,325 @@
+!!!!!!!!!!!!my programm !!!!!!!!!!!!!
+program simple
+
+  use LAMMPS_gether
+  use history
+  
+  implicit none
+  
+
+integer nstat,max_atom,nclu,n_entre
+parameter(nstat=125,max_atom=25001,nclu=2500,n_entre=15)
+
+!this data for exchange with LAMMPS program
+
+    !integer me - from LAMMPS_gether module
+    !integer natoms - from LAMMPS_gether module
+    !integer int_buf - from LAMMPS_gether module
+    !integer dbl_buf - from LAMMPS_gether module
+    integer  typ(max_atom), num_atom, num_vecino4(max_atom), num_vecino5(max_atom)
+    double precision pot(max_atom), kin(max_atom), vx(max_atom),vy(max_atom),vz(max_atom), & 
+                     x(max_atom),y(max_atom),z(max_atom)
+
+!this piece of text is devoted to statistical relations betw...
+    integer*4 statist(300), npromediar /300/,delta/5/, delta_tau, nstatist /10/
+    real*8 mpro,dpro,a
+    integer curnum1 /2/
+    logical :: sexist
+
+!
+    character*200 sfile 
+    character*150 sline
+
+! this data for cluster storage
+    integer*4 ncluster(max_atom),ncluster_o(max_atom),mcluster(0:nclu), & 
+              mcluster_o(0:nclu)  ! 1- только металл, 2 - есть аргон
+    integer cluster(0:nclu,1:nstat+1),cluster_o(0:nclu,1:nstat+1)
+
+
+!    integer*4 numj_all(1000), numt_t(1000),numj_o(1000),numjt(1000),numt_o(1000),numt_all(1000)
+!    integer   knumj_all, knumt_t, knumj_o, knumjt, knumt_o, knumt_all
+    
+    integer cyp(max_atom)
+
+    integer*4 i, j, jj, i1, k, nt, cur_clu , icl, iat, iat1, iat2
+    integer t, p, n, itwo,ipp, itres, ityp, ikk,ip,iexit,ifile,ik,n1,n2,n3
+        
+    integer*4  nmax /10000/,natraso /5/,sostav(500)
+    !integer cluster7_o(1:nclu,1:nstat),cluster7(1:nclu,1:nstat),mcluster7(0:nclu,2),ncluster7(1:natom),mcluster7_o(0:nclu,2),ncluster7_o(1:natom)
+
+    type evptr
+       type(event), pointer :: p =>null()
+    end type evptr
+
+    type (evptr), dimension(max_atom,5) ::  hist
+    type (event), pointer :: nev
+     
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+!--------------------------------------------------------
+CALL init_mpi_lammps('in.1') !!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+  
+nt=0
+mcluster(:) = 0        !numero de atoms en cada cluster
+ncluster(:) = 0        !numero de cluster que mantiene este atom 
+cluster(:,:) = 0          !numero del atom metallico
+
+!!me=1 ! DON'T forget to borrar it!
+  ! do something
+do i=1,5000
+  typ(i)=1
+enddo    
+do i=5001,max_atom
+   typ(i)=2
+enddo
+
+open(150,file='stat2.dat')
+open(44000,File='r1.dat')      
+
+do while(.true.)
+! entro y esta pensando
+!								if (me.eq.1) then 
+
+!!!!!!!!!!!!!!!!!!!!!!! read _data !!!!!!!!!!!!!!!!!!!!!!!!!
+!  do j=1,m
+!    num_atom,typ(num_atom),num_vecino4(num_atom),num_vecino5(num_atom),kin(num_atom), pot(num_atom)  !x(num_atom),y(num_atom),z(num_atom),
+!  enddo
+    call one_step_data(5)
+    if (me .ne. 0) cycle
+
+        j=1
+        jj=1
+        do i=1,natoms
+          num_atom=int_buf(j)
+          typ(num_atom) = int_buf(j+1) 
+          num_vecino4(num_atom) = int_buf(j+2) 
+          num_vecino5(num_atom) = int_buf(j+3)
+          j=j+4
+          
+          kin(num_atom) = dbl_buf(jj)
+          pot(num_atom) = dbl_buf(jj+1)
+	  x(num_atom) = dbl_buf(jj+2)
+	  y(num_atom) = dbl_buf(jj+3)
+	  z(num_atom) = dbl_buf(jj+4)
+	  vx(num_atom) = dbl_buf(jj+5)
+	  vy(num_atom) = dbl_buf(jj+6)
+	  vz(num_atom) = dbl_buf(jj+7)
+!          print  *, num_atom,x(num_atom),y(num_atom),z(num_atom),vx(num_atom),vy(num_atom),vz(num_atom)
+       
+          jj=jj+8
+
+        enddo
+        !print  *, '<<<<<<<<<<< read done  >>>>>>>>>>>>>>>>>'
+        nt=nt+1
+
+!!!!!!!!!!!!!!!!!!!!! check 1. my code 21.05.2015 !!!!!!!!!!!!!!!!1
+   open(5050,File='qqq')    !'//trim(sfile)//'
+   write(5050,*) nt			!real*8 kin(1: natom)
+   close(5050)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+
+
+!!!!!!!!!!!!!!!!!!!!! check 1. my code 21.05.2015 !!!!!!!!!!!!!!!!1
+!   write(sfile,'(i0)') (nt+1)*natraso
+!
+!   sfile=trim('dump/check_in'//trim(sfile)//'.bin')
+!   write(*,*) sfile
+!   open(5050,File=sfile,form='unformatted')    !'//trim(sfile)//'
+!
+!   write(5050) typ 			!integer typ(1:natom)
+!   write(5050) num_vecino5		!integer*4 num_vecino5(1:natom)
+!   write(5050) kin			!real*8 kin(1: natom)
+!   close(5050)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+if(int(1.*nt/nmax).eq. 1.*nt/nmax) then
+   close(44000)
+
+   !<<<<<<<<<<<<<<< statistics >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+   if((int(1.*(nt)/nstatist/nmax).eq. 1.*(nt)/nstatist/nmax) .and. nt .ne. 0) then
+   !if((int(1.*(nt-1)/nstatist/nmax).eq. 1.*(nt-1)/nstatist/nmax) .and. nt .ne. 1) then
+
+     statist=0 
+     a=0.
+     j=int(1.*nt/nmax)
+     do   ifile=j-nstatist + 1, j, 1
+       write(sfile,'(i0)') ifile
+       sfile=('r'//trim(sfile)//'.dat')
+       inquire (file=sfile,exist=sexist)
+       if(sexist) then
+         open(18,File=sfile,err=12) 
+
+         do while(.true.)    
+
+        !   read(18,('(2(i12,1x),a12,1x,a12,1x,3(i12,1x),125(i5,1x))'),end=11 ) ityp,n,sline,sline, n3, n1,n2, & 
+        !             (numjt(ik), ik=1,12)
+           if(sline.ne.'*******') then
+               read(sline,'(i12)') delta_tau
+           endif
+
+           if (curnum1==n .and. (ityp==1 .or. ityp==-1) .and. sline.ne.'*******') then    !ityp==1 .or. ityp==-1)
+               a=a+1.
+               do i=1,npromediar
+                  if (delta_tau>=(i-1.)*delta .and. delta_tau < i*delta) then
+                     statist(i)=statist(i)+1
+                     exit
+                  endif 
+               enddo 
+           endif
+
+         enddo
+     11  continue
+     
+       endif  ! if(sexist) then
+     enddo   !from  do   ifile=j-nstatist + 1, j, 1
+  12 continue
+
+     Mpro=0.
+     dpro=0.
+     do i=1,npromediar
+        Mpro=Mpro+statist(i)*(i*delta+delta/2.)/a
+        dpro=dpro+statist(i)*(i*delta+delta/2.)**2/a
+     enddo
+     dpro=dpro-mpro**2
+      ! write(14, '( 425(i5,1x))' ) (sargon(i),i=1,120)
+     write(150,('(i5,1x,3(F10.3, 1x), 425(i5,1x))')) ifile,a,mpro,dpro,(statist(i),i=1,npromediar)
+
+   endif    !from if(int(1.*nt/nstatist).eq. 1.*nt/nstatist) then
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<< end statistics >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+   write(sfile,'(i0)') int(nt/nmax)+1
+   !open(41000,File='osc'//trim(sfile)//'.dat') 
+   !open(15,File='t'//trim(sfile)//'.dat')      
+   open(44000,File='r'//trim(sfile)//'.dat')      
+   !open(42000,File='pro_argon'//trim(sfile)//'.dat')
+   ! open(43000,File='pro_argon_menos'//trim(sfile)//'.dat')      
+   ! open(47000,File='prot'//trim(sfile)//'.dat')      
+endif
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+!                do ifile=1,nmax
+!										    if (me.eq.1) then  
+ mcluster_o(:) = mcluster(:)
+ ncluster_o(:) = ncluster(:)
+ cluster_o(:,:) = cluster(:,:)
+
+ mcluster(:) = 0
+ ncluster(:) = 0
+ cluster(:,:) = 0
+ mcluster(0) = 1     ! нулевой кластер описывает все мономеры (размер 1) 
+ cur_clu=0
+
+
+ do iat = 1,natoms
+     if (typ(iat) .ne. 1) cycle
+     if (iat .eq. num_vecino5(iat)) cycle
+
+     icl = Ncluster(num_vecino5(iat))
+!     write(*,*) iat, num_vecino5(iat),typ(iat),icl
+     if (icl .eq. 0) then
+            cur_clu = cur_clu+1    !nuevo numero
+            icl = cur_clu
+            if (typ(num_vecino5(iat)) .eq. 1) then   ! add base atom
+               Ncluster(num_vecino5(iat)) = icl
+               mcluster(icl) = mcluster(icl)+1
+               cluster(icl,mcluster(icl)) = num_vecino5(iat)
+            endif
+     endif
+
+     Ncluster(iat) = icl
+     if (mcluster(icl) .lt. nstat) then
+        mcluster(icl) = mcluster(icl)+1
+        cluster(icl,mcluster(icl)) = iat
+     endif
+    ! write(*,*) iat, num_vecino5(iat),icl,Ncluster(iat),mcluster(icl)
+     
+ enddo    !!num_atom==1,m
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+cyp(:) = 0
+do j=1,5000  ! todos los atoms
+
+  if (nt .lt. 10) cycle
+  if (cyp(j) .eq. 1) cycle      ! skip atoms if they are in known clusters
+  if (typ(j) .ne. 1) cycle
+  if (mcluster(ncluster(j)) .eq. mcluster_o(ncluster_o(j))) cycle
+  
+  if (mcluster(ncluster(j)) < mcluster_o(ncluster_o(j))) then 
+!        write(*,*) 'stat: ! add dissociation'
+        if (ncluster(j) .eq. 0) cluster(ncluster(j),1) = j  ! мономер
+        
+        call add_diss(nev,cluster(ncluster(j),:), cluster_o(ncluster_o(j),:), nt)
+        do i=1,mcluster_o(ncluster_o(j))
+          iat = cluster_o(ncluster_o(j),i)
+          if (associated(hist(iat,5)%p)) then
+             write(*,*) 'stat: diss: write event to file!!!'
+             write(40,*) 'stat: diss:',hist(j,5)%p%part1,'+',hist(j,5)%p%part2,'==', hist(j,5)%p%time, hist(j,5)%p%fusion
+             
+             call rm_event(hist(j,5)%p)
+              
+          endif  
+          hist(iat,5)%p => hist(iat,4)%p
+          hist(iat,4)%p => hist(iat,3)%p
+          hist(iat,3)%p => hist(iat,2)%p
+          hist(iat,2)%p => hist(iat,1)%p
+          hist(iat,1)%p => nev
+          cyp(iat) = 1
+        enddo
+        
+  endif
+
+  if (mcluster(ncluster(j)) > mcluster_o(ncluster_o(j))) then 
+!        write(*,*) 'stat: Add fusion',j
+        if (ncluster_o(j) .eq. 0) cluster_o(ncluster_o(j),1) = j  ! мономер
+        call add_fusion(nev,cluster(ncluster(j),:), cluster_o(ncluster_o(j),:), nt)
+    
+!------ check for loops ---------
+        if (loop0(nev,hist(j,1)%p) == 1) then 
+           write(*,*) 'stat: Simple loop. Remove last step (hist(j))'
+           call rm_event(hist(j,1)%p)
+           do i = 1, mcluster(ncluster(j))
+              iat = cluster(ncluster(j),i)
+              hist(iat,1)%p => hist(iat,2)%p
+              hist(iat,2)%p => hist(iat,3)%p
+              hist(iat,3)%p => hist(iat,4)%p
+              hist(iat,4)%p => hist(iat,5)%p
+              hist(iat,5)%p => NULL()
+           enddo
+        else
+           iat1 = nev%part1(1)
+           iat2 = nev%part2(1)
+           if (loop1(nev,hist(iat1,1)%p,hist(iat2,1)%p) == 1) then
+               write(*,*) 'stat: Complex loop. reconstruction of history'
+           
+           else
+              write(*,*) 'stat: No loops. Write new history'
+              do i = 1,mcluster(ncluster(j))
+                 iat = cluster(ncluster(j),i)
+                 if (associated(hist(iat,5)%p)) then
+                    write (*,*) 'stat: fusion: write event to file!!!'
+                 endif  
+                 hist(iat,5)%p => hist(iat,4)%p
+                 hist(iat,4)%p => hist(iat,3)%p
+                 hist(iat,3)%p => hist(iat,2)%p
+                 hist(iat,2)%p => hist(iat,1)%p
+                 hist(iat,1)%p => nev
+              enddo
+           endif
+        endif
+ 
+        do i = 1,mcluster(ncluster(j))
+           iat = cluster(ncluster(j),i)
+           cyp(iat) = 1
+        enddo
+  endif
+
+enddo   !form j=1,5000
+
+enddo    !from do while .true. <<<<<< end of main circle
+
+
+call finalize_mpi_lammps()
+10 continue
+
+end program 
+
